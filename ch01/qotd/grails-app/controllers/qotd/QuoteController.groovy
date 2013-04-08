ecf20dceaa26fe1b10b1c0f7e0b63c1aacdaf76b
yoyojyv <yoyojyv@gmail.com>
2013-04-08 19:40:00 +0900

    set layout and basic controller

---
+++ ch01/qotd/grails-app/controllers/qotd/QuoteController.groovy
@@ -0,0 +1,19 @@
+package qotd
+
+class QuoteController {
+
+    def index() {
+        redirect(action: "home")
+    }
+
+    def home() {
+        render "<h1>Real Programmers do not eat Quiche</h1>"
+    }
+
+    def random() {
+        def staticAuthor = "Anonymous"
+        def staticContent = "Real Programmers don't eat much quiche"
+        [ author: staticAuthor, content: staticContent]
+    }
+
+}
