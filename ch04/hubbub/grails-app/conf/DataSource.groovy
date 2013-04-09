02500c9be066783f00cf11101ec21c89c64c6061
yoyojyv <yoyojyv@gmail.com>
2013-04-09 16:13:56 +0900

    add config (readable hibernate sql log)

--- ch04/hubbub/grails-app/conf/DataSource.groovy
+++ ch04/hubbub/grails-app/conf/DataSource.groovy
@@ -8,6 +8,8 @@ hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
     cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
+    format_sql = true
+    use_sql_comments = true
 }
 // environment specific settings
 environments {
